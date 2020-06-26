48b02115a2c9e2104eb84acc886989bdefa79a9f
Ken Longnan <ken.longnan@gmail.com>
2020-06-27 00:33:06 +0800

    ...

--- kbs-setup.sh
+++ kbs-setup.sh
@@ -4,8 +4,18 @@ echo "------------------------------------------------------------"
 echo "####### Setup KBS Docker Suite"
 echo "------------------------------------------------------------"
 
-DIR="$(dirname "${BASH_SOURCE[0]}")"  # get the directory name
-source $DIR/env.sh
+KBS_TAG=7.1.0.latest
+KBS_DEBUG=true
+
+KBS_DB_HOST=kbs-pgserver
+KBS_DB_PORT=5432
+KBS_DB_NAME=idempiere_db
+KBS_DB_USER=adempiere
+KBS_DB_PASS=adempiere
+
+PORT_KBS=8080
+PORT_PORTAINER=9000
+PORT_PGADMIN4=9010
 
 echo "------------------------------------------------------------"
 echo "###### Creating data volume..."
