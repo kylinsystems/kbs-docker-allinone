9810c22e62cbc5e07ce6cf8c71c411e4a1c9dd60
Ken Longnan <ken.longnan@gmail.com>
2020-06-27 00:40:35 +0800

    ...

--- kbs-setup.sh
+++ kbs-setup.sh
@@ -4,18 +4,18 @@ echo "------------------------------------------------------------"
 echo "####### Setup KBS Docker Suite"
 echo "------------------------------------------------------------"
 
-KBS_TAG=7.1.0.latest
-KBS_DEBUG=true
+export KBS_TAG=7.1.0.latest
+export BS_DEBUG=true
 
-KBS_DB_HOST=kbs-pgserver
-KBS_DB_PORT=5432
-KBS_DB_NAME=idempiere_db
-KBS_DB_USER=adempiere
-KBS_DB_PASS=adempiere
+export KBS_DB_HOST=kbs-pgserver
+export KBS_DB_PORT=5432
+export KBS_DB_NAME=idempiere_db
+export KBS_DB_USER=adempiere
+export KBS_DB_PASS=adempiere
 
-PORT_KBS=8080
-PORT_PORTAINER=9000
-PORT_PGADMIN4=9010
+export ORT_KBS=8080
+export PORT_PORTAINER=9000
+export PORT_PGADMIN4=9010
 
 echo "------------------------------------------------------------"
 echo "###### Creating data volume..."
