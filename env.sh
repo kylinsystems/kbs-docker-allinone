7c862b6703e6d5f24d3225cebbac2979eaeedece
Ken Longnan <ken.longnan@gmail.com>
2020-06-27 00:00:20 +0800

    ...

--- env.sh
+++ env.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 KBS_TAG=7.1.0.latest
+export KBS_TAG
+
 KBS_DEBUG=true
+export KBS_DEBUG
 
 KBS_DB_HOST=kbs-pgserver
+export KBS_DB_HOST
+
 KBS_DB_PORT=5432
+export KBS_DB_PORT
+
 KBS_DB_NAME=idempiere_db
+export KBS_DB_NAME
+
 KBS_DB_USER=adempiere
+export KBS_DB_USER
+
 KBS_DB_PASS=adempiere
+export KBS_DB_PASS
 
 PORT_KBS=8080
+export PORT_KBS
+
 PORT_PORTAINER=9000
+export PORT_PORTAINER
+
 PORT_PGADMIN4=9010
+export PORT_PGADMIN4
